abc6ee18a38dfb6f38168a3ef8b773a6a077bfd5
kurtgalvin <45803439+kurtgalvin@users.noreply.github.com>
2019-10-28 22:20:44 -0700

    Indicator ready

--- trading_workbench/core.py
+++ trading_workbench/core.py
@@ -1,13 +1,102 @@
 import pandas as pd
 
 
+class BackTest:
+    def __init__(self, strategy, data):
+        x = strategy(data)
+        print(x.sma)
+        print(x.sma.now)
+        print(x.sma.prev)
+
+
 class Strategy:
     def __init__(self, data):
-        self.df = pd.DataFrame(data)
+        df = pd.DataFrame(data)
 
-    def __init_subclass__(self):
+        largest_prev = 0
+        for v in self._indicators.values():
+            v.build(df)
+            if v.prev_count > largest_prev:
+                largest_prev = v.prev_count
+        
+        largest_valid = 0
+        for i in df.iteritems():
+            first = i[1].first_valid_index()
+            if first > largest_valid:
+                largest_valid = first
+
+        first_index = largest_prev + largest_valid
+        for v in self._indicators.values():
+            v.index = first_index
+
+    def open(self, long=False, short=False):
+        if long:
+            pass
+        elif short:
+            pass
+
+    def close(self):
         pass
+
+    def __init_subclass__(cls):
+        indicators = {}
+        for k, v in cls.__dict__.items():
+            if isinstance(v, Indicator):
+                indicators[k] = v
+        setattr(cls, '_indicators', indicators)
 
 
 class Indicator:
-    pass
+    def __init__(self, indicator, params=None, prev=1):
+        self.indicator = indicator
+        self.params = params
+        self.prev_count = prev
+        self._index = None
+        self._result = None
+
+    def build(self, df):
+        self._result = self.indicator(df, *self.params)
+        return self._result
+
+    @property
+    def index(self):
+        return self._index
+
+    @index.setter
+    def index(self, value):
+        self._index = value
+
+    @property
+    def now(self):
+        return self._result.iloc[self._index]
+
+    @property
+    def prev(self):
+        return self._result.iloc[self._index-self.prev_count:self._index].iloc[::-1]
+
+    def __str__(self):
+        return str(self._result)
+
+
+def SMA(df, n):
+    df['sma'] = df['close'].rolling(n).mean()
+    return df['sma']
+
+class TestStrategy(Strategy):
+    sma = Indicator(SMA, params=(2,), prev=2)
+
+    def open_trade(self):
+        pass
+
+    def close_trade(self):
+        pass
+
+
+if __name__ == '__main__':
+    data = {
+        'open': [1,2,3,4,3,2,1,2,3,4,3,2,1],
+        'high': [2,3,4,5,4,3,2,3,4,5,4,3,2],
+        'low': [1,2,3,4,3,2,1,2,3,4,3,2,1],
+        'close': [1.5,2.5,3.5,4.5,3.5,2.5,1.5,2.5,3.5,4.5,3.5,2.5,1.5]
+    }
+    x = BackTest(TestStrategy, data)
--- trading_workbench/core_test.py
+++ trading_workbench/core_test.py
@@ -1,20 +1,43 @@
 import pytest
 import pandas as pd
 
-from core import Strategy
+from core import Strategy, Indicator
 
 @pytest.fixture
 def build_strategy():
+    class SMA(Indicator):
+        def __init__(self, n):
+            self.n = n
+    
+        def __get__(self, strategy, owner):
+            close = strategy.df['close']
+            return close.rolling(self.n).mean()
+
     class TestStrategy(Strategy):
-        pass
+        sma = SMA(2)
     
     data = {
-        'open': [1,2,3,4,3,2,1],
-        'high': [2,3,4,5,4,3,2],
-        'low': [1,2,3,4,3,2,1],
-        'close': [1.5,2.5,3.5,4.5,3.5,2.5,1.5]
+        'open': [1,2,3,4,3,2,1,2,3,4,3,2,1],
+        'high': [2,3,4,5,4,3,2,3,4,5,4,3,2],
+        'low': [1,2,3,4,3,2,1,2,3,4,3,2,1],
+        'close': [1.5,2.5,3.5,4.5,3.5,2.5,1.5,2.5,3.5,4.5,3.5,2.5,1.5]
     }
     return TestStrategy(data)
 
 def test_df(build_strategy):
     assert isinstance(build_strategy.df, pd.DataFrame), "`df` is not `DataFrame`"
+
+def test_strategy_indicators(build_strategy):
+    assert hasattr(build_strategy, '_indicators'), "No `_indicators`"
+    assert isinstance(build_strategy._indicators, dict), "`_indicators` is not dict"
+    assert build_strategy._indicators.get('sma', False), "`_indicators` has no `sma`"
+
+def test_dataframe(build_strategy):
+    df = pd.DataFrame({
+        'open': [1,2,3,4,3,2,1,2,3,4,3,2,1],
+        'high': [2,3,4,5,4,3,2,3,4,5,4,3,2],
+        'low': [1,2,3,4,3,2,1,2,3,4,3,2,1],
+        'close': [1.5,2.5,3.5,4.5,3.5,2.5,1.5,2.5,3.5,4.5,3.5,2.5,1.5],
+        'sma': [None, 2.0, 3.0, 4.0, 4.0, 3.0, 2.0, 2.0, 3.0, 4.0, 4.0, 3.0, 2.0]
+    })
+    assert df.equals(build_strategy.df), "dataframe does not match"
